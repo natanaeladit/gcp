1dd00bcf16c25b3e67123cf2a5021c7fc937c4c5
NatanaelA <natanaela@xrvision.com>
2019-07-14 12:29:12 +0800

    add integration test

---
+++ src/test/java/io/vertx/web/RestIT.java
@@ -0,0 +1,47 @@
+package io.vertx.web;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestIT {
+    @BeforeClass
+    public static void configureRestAssured() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = Integer.getInteger("http.port", 9999);
+    }
+
+    @AfterClass
+    public static void unconfigureRestAssured() {
+        RestAssured.reset();
+    }
+
+    @Test
+    public void checkThatWeCanRetrieveIndividualProduct() {
+        final int id = get("/api/whiskies").then().assertThat().statusCode(200).extract().jsonPath()
+                .getInt("find { it.name=='Bowmore 15 Years Laimrig' }.id");
+        get("/api/whiskies/" + id).then().assertThat().statusCode(200).body("name", equalTo("Bowmore 15 Years Laimrig"))
+                .body("origin", equalTo("Scotland, Islay")).body("id", equalTo(id));
+    }
+
+    @Test
+    public void checkWeCanAddAndDeleteAProduct() {
+        Whisky whisky = given().body("{\"name\":\"Jameson\", \"origin\":\"Ireland\"}").request().post("/api/whiskies")
+                .thenReturn().as(Whisky.class);
+        assertThat(whisky.getName()).isEqualToIgnoringCase("Jameson");
+        assertThat(whisky.getOrigin()).isEqualToIgnoringCase("Ireland");
+        assertThat(whisky.getId()).isNotZero();
+        
+        get("/api/whiskies/" + whisky.getId()).then().assertThat().statusCode(200).body("name", equalTo("Jameson"))
+                .body("origin", equalTo("Ireland")).body("id", equalTo(whisky.getId()));
+                
+        delete("/api/whiskies/" + whisky.getId()).then().assertThat().statusCode(204);
+        
+        get("/api/whiskies/" + whisky.getId()).then().assertThat().statusCode(404);
+    }
+}
--- src/test/java/io/vertx/web/VerticleTest.java
+++ src/test/java/io/vertx/web/VerticleTest.java
@@ -2,6 +2,8 @@ package io.vertx.web;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -49,5 +51,38 @@ public class VerticleTest {
                 async.complete();
             });
         });
+    }
+
+    @Test
+    public void checkThatTheIndexPageIsServed(TestContext context) {
+        final Async async = context.async();
+
+        vertx.createHttpClient().getNow(port, "localhost", "/assets/index.html", response -> {
+            context.assertEquals(response.statusCode(), 200);
+            context.assertTrue(response.headers().get("content-type").contains("text/html"));
+            response.bodyHandler(body -> {
+                context.assertTrue(body.toString().contains("<h1>My Whisky Collection</h1>"));
+                async.complete();
+            });
+        });
+    }
+
+    @Test
+    public void checkThatWeCanAdd(TestContext context) {
+        Async async = context.async();
+        final String json = Json.encodePrettily(new Whisky("Jameson", "Ireland"));
+        final String length = Integer.toString(json.length());
+        vertx.createHttpClient().post(port, "localhost", "/api/whiskies").putHeader("content-type", "application/json")
+                .putHeader("content-length", length).handler(response -> {
+                    context.assertEquals(response.statusCode(), 201);
+                    context.assertTrue(response.headers().get("content-type").contains("application/json"));
+                    response.bodyHandler(body -> {
+                        final Whisky whisky = Json.decodeValue(body.toString(), Whisky.class);
+                        context.assertEquals(whisky.getName(), "Jameson");
+                        context.assertEquals(whisky.getOrigin(), "Ireland");
+                        context.assertNotNull(whisky.getId());
+                        async.complete();
+                    });
+                }).write(json).end();
     }
 }
